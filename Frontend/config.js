fb3675e28935523559a4c364cf185d34e099173a
Anu K <kamateanushka@gmail.com>
2026-04-21 00:51:51 +0530

    FINAL FIX: removed localhost issue

--- Frontend/config.js
+++ Frontend/config.js
@@ -3,5 +3,5 @@
  * Change API_BASE_URL to your deployed backend URL in production.
  */
 const CONFIG = {
-    API_BASE_URL: "http://localhost:5000"
+    API_BASE_URL: "https://smart-foodplate.onrender.com"
 };
